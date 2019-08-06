e6c07794bb2e7a504e322ad60ba6498738957f0a
Kirill Tolkun <tolkun_14@inbox.ru>
2019-08-07 02:09:36 +0300

    Refactor sql SELECT queries

--- src/by/tolkun/barbershop/dao/mysql/ReservationDaoImpl.java
+++ src/by/tolkun/barbershop/dao/mysql/ReservationDaoImpl.java
@@ -203,7 +203,8 @@ public class ReservationDaoImpl extends BaseDaoImpl implements ReservationDao {
                 + "JOIN offers ON offers.id = reservations.offer_id "
                 + "JOIN users AS customers ON customers.id = reservations.customer_id "
                 + "JOIN users AS employees ON employees.id = reservations.employee_id "
-                + "JOIN employees AS employees_info ON employees.id = employees.id;";
+                + "JOIN employees AS employees_info ON employees.id = employees.id "
+                + "GROUP BY offer_id;";
         ResultSet resultSet = null;
         PreparedStatement statement = null;
         try {
@@ -273,45 +274,47 @@ public class ReservationDaoImpl extends BaseDaoImpl implements ReservationDao {
 
     @Override
     public List<Reservation> readByCustomer(final int customerId) throws PersistentException {
-        final String query = "SELECT "
-                + "reservations.id,"
-                + "offers.id AS offer_id,"
-                + "offers.name AS offer_name,"
-                + "offers.description AS offer_description,"
-                + "offers.price AS offer_price,"
-                + "offers.period AS offer_period,"
-                + "customers.id AS customer_id,"
-                + "customers.login AS customer_login,"
-                + "customers.password AS customer_password,"
-                + "customers.name AS customer_name,"
-                + "customers.surname AS customer_surname,"
-                + "customers.patronymic AS customer_patronymic,"
-                + "customers.email AS customer_email,"
-                + "customers.phone AS customer_phone,"
-                + "customers.image_path AS customer_image_path,"
-                + "customers.role AS customer_role,"
-                + "employees.id AS employee_id,"
-                + "employees.login AS employee_login,"
-                + "employees.password AS employee_password,"
-                + "employees.name AS employee_name,"
-                + "employees.surname AS employee_surname,"
-                + "employees.patronymic AS employee_patronymic,"
-                + "employees.email AS employee_email,"
-                + "employees.phone AS employee_phone,"
-                + "employees.image_path AS employee_image_path,"
-                + "employees.role AS employee_role,"
-                + "employees_info.experience AS employee_experience, "
-                + "reservations.date "
-                + "FROM reservations "
-                + "JOIN offers ON offers.id = reservations.offer_id "
-                + "JOIN users AS customers ON customers.id = reservations.customer_id "
-                + "JOIN users AS employees ON employees.id = reservations.employee_id "
-                + "JOIN employees AS employees_info ON employees.id = employees.id "
-                + "WHERE reservations.customer_id = ?";
+        final String query = "SELECT " +
+                "reservations.id, " +
+                "offers.id AS offer_id, " +
+                "offers.name AS offer_name, " +
+                "offers.description AS offer_description, " +
+                "offers.price AS offer_price, " +
+                "offers.period AS offer_period, " +
+                "customers.id AS customer_id, " +
+                "customers.login AS customer_login, " +
+                "customers.password AS customer_password, " +
+                "customers.name AS customer_name, " +
+                "customers.surname AS customer_surname, " +
+                "customers.patronymic AS customer_patronymic, " +
+                "customers.email AS customer_email, " +
+                "customers.phone AS customer_phone, " +
+                "customers.image_path AS customer_image_path, " +
+                "customers.role AS customer_role, " +
+                "employees.id AS employee_id, " +
+                "employees.login AS employee_login, " +
+                "employees.password AS employee_password, " +
+                "employees.name AS employee_name, " +
+                "employees.surname AS employee_surname, " +
+                "employees.patronymic AS employee_patronymic, " +
+                "employees.email AS employee_email, " +
+                "employees.phone AS employee_phone, " +
+                "employees.image_path AS employee_image_path, " +
+                "employees.role AS employee_role, " +
+                "employees_info.experience AS employee_experience, " +
+                "reservations.date " +
+                "FROM reservations " +
+                "JOIN offers ON offers.id = reservations.offer_id " +
+                "JOIN users AS customers ON customers.id = reservations.customer_id " +
+                "JOIN users AS employees ON employees.id = reservations.employee_id " +
+                "JOIN employees AS employees_info ON employees.id = employees.id " +
+                "WHERE reservations.customer_id = ? " +
+                "GROUP BY offer_id;";
         ResultSet resultSet = null;
         PreparedStatement statement = null;
         try {
             statement = connection.prepareStatement(query);
+            statement.setInt(1, customerId);
             resultSet = statement.executeQuery();
             ReservationBuilder reservationBuilder = new ReservationBuilder();
             OfferBuilder offerBuilder = new OfferBuilder();
@@ -411,11 +414,13 @@ public class ReservationDaoImpl extends BaseDaoImpl implements ReservationDao {
                 + "JOIN users AS customers ON customers.id = reservations.customer_id "
                 + "JOIN users AS employees ON employees.id = reservations.employee_id "
                 + "JOIN employees AS employees_info ON employees.id = employees.id "
-                + "WHERE reservations.employee_id = ?";
+                + "WHERE reservations.employee_id = ?"
+                + "GROUP BY offer_id;";
         ResultSet resultSet = null;
         PreparedStatement statement = null;
         try {
             statement = connection.prepareStatement(query);
+            statement.setInt(1, employeeId);
             resultSet = statement.executeQuery();
             ReservationBuilder reservationBuilder = new ReservationBuilder();
             OfferBuilder offerBuilder = new OfferBuilder();
@@ -510,12 +515,12 @@ public class ReservationDaoImpl extends BaseDaoImpl implements ReservationDao {
             statement.setInt(1, id);
             statement.executeUpdate();
         } catch (SQLException e) {
-            e.printStackTrace();
+            LOGGER.error(e);
         } finally {
             try {
                 statement.close();
             } catch (SQLException | NullPointerException e) {
-                LOGGER.error(e);;
+                LOGGER.error(e);
             }
         }
     }
