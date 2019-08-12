554bc32ad6b81c34af0a9f4f573c75574a845698
Kirill Tolkun <tolkun_14@inbox.ru>
2019-08-13 00:10:36 +0300

    Add logger in main, menu, staff actions

--- src/by/tolkun/barbershop/action/MainAction.java
+++ src/by/tolkun/barbershop/action/MainAction.java
@@ -1,9 +1,15 @@
 package by.tolkun.barbershop.action;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class MainAction extends Action {
+    private static final Logger LOGGER
+            = LogManager.getLogger(MainAction.class);
+
     @Override
     public Forward execute(HttpServletRequest request, HttpServletResponse response) {
         return new Forward("/index.jsp", false);
--- src/by/tolkun/barbershop/action/MenuItem.java
+++ src/by/tolkun/barbershop/action/MenuItem.java
@@ -1,35 +1,39 @@
 package by.tolkun.barbershop.action;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.Serializable;
 
 public class MenuItem implements Serializable {
-	private String url;
-	private String name;
-	private String icon;
+    private static Logger logger = LogManager.getLogger(MenuItem.class);
+    private String url;
+    private String name;
+    private String icon;
 
-	public MenuItem(final String inputUrl,
-					final String inputName) {
-		url = inputUrl;
-		name = inputName;
-	}
+    public MenuItem(final String inputUrl,
+                    final String inputName) {
+        url = inputUrl;
+        name = inputName;
+    }
 
-	public MenuItem(final String inputUrl,
-					final String inputName,
-					final String inputIcon) {
-		url = inputUrl;
-		name = inputName;
-		icon = inputIcon;
-	}
+    public MenuItem(final String inputUrl,
+                    final String inputName,
+                    final String inputIcon) {
+        url = inputUrl;
+        name = inputName;
+        icon = inputIcon;
+    }
 
-	public String getUrl() {
-		return url;
-	}
+    public String getUrl() {
+        return url;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public String getIcon() {
-		return icon;
-	}
+    public String getIcon() {
+        return icon;
+    }
 }
