7f93d3311de4f471e810e76e3293d39605c276f7
Kirill Tolkun <tolkun_14@inbox.ru>
2019-10-14 01:06:08 +0300

    Add SpringConfig class (configuratin class for spring)

---
+++ src/main/java/by/tolkun/barbershop/config/SpringConfig.java
@@ -0,0 +1,29 @@
+package by.tolkun.barbershop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan(basePackages = "by.tolkun.barbershop")
+public class SpringConfig {
+
+    @Bean
+    public JdbcTemplate getJdbcTemplate(){
+        return new JdbcTemplate(getDataSource());
+    }
+
+    @Bean
+    public DataSource getDataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost:3306/barbershop_db");
+        dataSource.setUsername("barbershop_user");
+        dataSource.setPassword("barber");
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        return dataSource;
+    }
+}
