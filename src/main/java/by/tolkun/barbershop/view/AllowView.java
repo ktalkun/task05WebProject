42be894482f8667502ef38091ce6fcb98f6d8952
Kirill Tolkun <tolkun_14@inbox.ru>
2019-10-20 13:02:22 +0300

    Add AllowView class with constants allow views

---
+++ src/main/java/by/tolkun/barbershop/view/AllowView.java
@@ -0,0 +1,18 @@
+package by.tolkun.barbershop.view;
+
+public final class AllowView {
+    public static final String MESSAGE = "message";
+    public static final String ERROR = "error";
+    public static final String LOGIN = "login";
+    public static final String SIGNIN = "signin";
+    public static final String ROOT = "";
+    public static final String INDEX = "index";
+    public static final String SERVICE = "service";
+    public static final String BARBER = "barber";
+    public static final String STAFF = "staff";
+    public static final String ABOUT = "about";
+    public static final String BOOK = "book";
+    public static final String PROFILE_EDIT = "profile/edit";
+
+    private AllowView(){}
+}
