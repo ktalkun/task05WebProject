832d2190c2b6e75d175b6feba2b4656a4c6bbd5e
Kirill Tolkun <tolkun_14@inbox.ru>
2019-10-20 12:41:48 +0300

    Refactor. Delete EncodingFilter (use CharacterEncodingFilter from springframework). Replace SecurityFilter

--- src/main/java/by/tolkun/barbershop/filter/SecurityFilter.java
+++ src/main/java/by/tolkun/barbershop/filter/SecurityFilter.java
@@ -1,77 +1,92 @@
-package by.tolkun.barbershop.controller;
+package by.tolkun.barbershop.filter;
 
-import by.tolkun.barbershop.action.Action;
 import by.tolkun.barbershop.entity.Role;
 import by.tolkun.barbershop.entity.User;
+import by.tolkun.barbershop.role.AllowRole;
+import by.tolkun.barbershop.url.AllowPageURL;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.web.filter.GenericFilterBean;
 
-import javax.servlet.*;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.Set;
 
-public class SecurityFilter implements Filter {
+public class SecurityFilter extends GenericFilterBean {
+    private static final String MESSAGE_PAGE_URL = "/message.html";
     private static Logger LOGGER = LogManager.getLogger(SecurityFilter.class);
-
-    private static final String MESSAGE_PAGE_URL = "/message.jsp";
-
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-    }
 
     @Override
     public void doFilter(final ServletRequest request,
                          final ServletResponse response,
                          final FilterChain chain)
             throws IOException, ServletException {
+        LOGGER.debug("Security filter");
         if (request instanceof HttpServletRequest
                 && response instanceof HttpServletResponse) {
             String message = null;
-            String redirectUrl = "/book.jsp";
+            String redirectUrl = AllowPageURL.LOGIN;
+
             HttpServletRequest httpRequest = (HttpServletRequest) request;
             HttpServletResponse httpResponse = (HttpServletResponse) response;
-            Action action = (Action) httpRequest.getAttribute("action");
-            Set<Role> allowRoles = action.getAllowRoles();
+            httpRequest.setAttribute("message",
+                    httpRequest.getSession().getAttribute("message"));
+            httpRequest.setAttribute("redirectUrl",
+                    httpRequest.getSession().getAttribute("redirectUrl"));
+
+            String currentPageURL
+                    = getCurrentRelativePageURL(httpRequest);
+            Set<Role> allowRoles = AllowRole.getAllowRolesByURL(currentPageURL);
             String userName = "unauthorized user";
             HttpSession session = httpRequest.getSession(false);
             User user = null;
             if (session != null) {
                 user = (User) session.getAttribute("authorizedUser");
             }
-            LOGGER.debug(allowRoles);
-            boolean canExecute = allowRoles.size() == 0;
+            LOGGER.debug("Current page url: {}", currentPageURL);
+            LOGGER.debug("Allow roles: {}", allowRoles);
+            boolean canExecute = allowRoles == null
+                    || allowRoles.contains(Role.GUEST);
             if (user != null) {
                 userName = user.getLogin();
                 canExecute = canExecute || allowRoles.contains(user.getRole());
             }
-            LOGGER.debug("after check user {}", canExecute);
+            LOGGER.debug("Can execute: {}", canExecute);
             if (canExecute) {
                 chain.doFilter(request, response);
-                LOGGER.debug("after chain dofilter");
+                LOGGER.debug("After chain dofilter");
             } else {
                 LOGGER.info("Trying of \"{}\" access to forbidden resource \"{}\"",
-                        userName, action.getName());
+                        userName, currentPageURL);
                 message = "You can't get access to this page as unauthorized user. Log in or sign in please.";
-                ((HttpServletRequest) request).getSession().setAttribute("redirectUrl", "/login.jsp");
-                ((HttpServletRequest) request).getSession().setAttribute("message", message);
                 httpRequest
                         .getSession()
                         .setAttribute("message", message);
                 httpRequest
                         .getSession()
                         .setAttribute("redirectUrl", redirectUrl);
-                httpResponse.sendRedirect(httpRequest.getContextPath() + MESSAGE_PAGE_URL);
+                httpResponse.sendRedirect(httpRequest.getContextPath()
+                        + MESSAGE_PAGE_URL);
             }
         } else {
             LOGGER.error("It is impossible to use HTTP filter");
-            request.getServletContext().getRequestDispatcher("/jsp/error.jsp").forward(request, response);
+            request
+                    .getServletContext()
+                    .getRequestDispatcher("/jsp/error.jsp")
+                    .forward(request, response);
         }
     }
 
-    @Override
-    public void destroy() {
+    private String getCurrentRelativePageURL(final HttpServletRequest request) {
+        String contextPath = request.getContextPath();
+        String URI = request.getRequestURI();
+        int beginAction = contextPath.length();
+        return URI.substring(beginAction);
     }
 }
