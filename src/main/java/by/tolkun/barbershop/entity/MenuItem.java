567d9d8dd37ee78f041cef8abdb2805d18540472
Kirill Tolkun <tolkun_14@inbox.ru>
2019-10-20 12:49:12 +0300

    Refacotr. Return MenuItem class, that was deleted with folder actions in previous commits

---
+++ src/main/java/by/tolkun/barbershop/entity/MenuItem.java
@@ -0,0 +1,39 @@
+package by.tolkun.barbershop.entity;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.Serializable;
+
+public class MenuItem implements Serializable {
+    private static Logger logger = LogManager.getLogger(MenuItem.class);
+    private String url;
+    private String name;
+    private String icon;
+
+    public MenuItem(final String inputUrl,
+                    final String inputName) {
+        url = inputUrl;
+        name = inputName;
+    }
+
+    public MenuItem(final String inputUrl,
+                    final String inputName,
+                    final String inputIcon) {
+        url = inputUrl;
+        name = inputName;
+        icon = inputIcon;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+}
