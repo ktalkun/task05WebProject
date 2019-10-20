987a70a79dfc6042c7aac5221857183af9a37e87
Kirill Tolkun <tolkun_14@inbox.ru>
2019-10-20 12:59:58 +0300

    Add AllowPageURL class with constants allow urls of page

---
+++ src/main/java/by/tolkun/barbershop/url/AllowPageURL.java
@@ -0,0 +1,22 @@
+package by.tolkun.barbershop.url;
+
+/**
+ * Set of allow relative page urls
+ */
+public final class AllowPageURL {
+    public static final String MESSAGE = "/message.html";
+    public static final String ERROR = "/error.html";
+    public static final String LOGIN = "/login.html";
+    public static final String LOGOUT = "/logout.html";
+    public static final String SIGNIN = "/signin.html";
+    public static final String ROOT = "/";
+    public static final String INDEX = "/index.html";
+    public static final String SERVICE = "/service.html";
+    public static final String BARBER = "/barber.html";
+    public static final String STAFF = "/staff.html";
+    public static final String ABOUT = "/about.html";
+    public static final String BOOK = "/book.html";
+    public static final String PROFILE_EDIT = "/profile/edit.html";
+
+    private AllowPageURL(){}
+}
