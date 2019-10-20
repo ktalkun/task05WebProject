090fb6ae6057290bc32c0517155cc094fa877a33
Kirill Tolkun <tolkun_14@inbox.ru>
2019-10-20 12:21:33 +0300

    Refactor. Add annotation @Service, and @Autowired for field reviewDao

--- src/main/java/by/tolkun/barbershop/service/impl/ReviewServiceImpl.java
+++ src/main/java/by/tolkun/barbershop/service/impl/ReviewServiceImpl.java
@@ -1,20 +1,23 @@
 package by.tolkun.barbershop.service.impl;
 
-import by.tolkun.barbershop.dao.DAOFactory;
 import by.tolkun.barbershop.dao.ReviewDao;
 import by.tolkun.barbershop.entity.Review;
 import by.tolkun.barbershop.exception.LogicException;
 import by.tolkun.barbershop.exception.PersistentException;
 import by.tolkun.barbershop.service.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ReviewServiceImpl implements ReviewService {
 
     private ReviewDao reviewDao;
 
-    public ReviewServiceImpl(){
-        reviewDao = DAOFactory.getInstance().getReviewDao();
+    @Autowired
+    public ReviewServiceImpl(final ReviewDao reviewDao) {
+        this.reviewDao = reviewDao;
     }
 
     @Override
