36120b66e982748b49e474da203143555fcc6758
Kirill Tolkun <tolkun_14@inbox.ru>
2019-10-20 12:44:40 +0300

    Add CharacterEncodingFilter (from springframework) and SecurityFilter

--- src/main/java/by/tolkun/barbershop/config/ApplicationInitializer.java
+++ src/main/java/by/tolkun/barbershop/config/ApplicationInitializer.java
@@ -1,11 +1,14 @@
 package by.tolkun.barbershop.config;
 
+import by.tolkun.barbershop.filter.SecurityFilter;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.DispatcherServlet;
 
+import javax.servlet.FilterRegistration;
 import javax.servlet.MultipartConfigElement;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletRegistration;
@@ -14,11 +17,21 @@ public class ApplicationInitializer implements WebApplicationInitializer {
     private String TMP_FOLDER = "/";
     private int MAX_UPLOAD_SIZE = 5 * 1024 * 1024;
 
+
     @Override
     public void onStartup(ServletContext context) {
         AnnotationConfigWebApplicationContext root = new AnnotationConfigWebApplicationContext();
         root.scan("by.tolkun.barbershop");
         context.addListener(new ContextLoaderListener(root));
+
+        FilterRegistration.Dynamic encodingFilter = context.addFilter("encodingFilter",
+                new CharacterEncodingFilter());
+        encodingFilter.setInitParameter("encoding", "UTF-8");
+        encodingFilter.setInitParameter("forceEncoding", "true");
+        encodingFilter.addMappingForUrlPatterns(null, true, "/*");
+
+        context.addFilter("securityFilter", new SecurityFilter()).addMappingForUrlPatterns(null, true, "/*" );
+
         ServletRegistration.Dynamic appServlet =
                 context.addServlet("mvc", new DispatcherServlet(new GenericWebApplicationContext()));
         appServlet.setLoadOnStartup(1);
