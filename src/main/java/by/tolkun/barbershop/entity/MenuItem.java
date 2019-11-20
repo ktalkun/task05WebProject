735ed31cc7aecc81f1fe484c06ab33f6e0d1076e
Kirill Tolkun <tolkun_14@inbox.ru>
2019-11-21 01:40:33 +0300

    Add methods equals and hashCode

--- src/main/java/by/tolkun/barbershop/entity/MenuItem.java
+++ src/main/java/by/tolkun/barbershop/entity/MenuItem.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class MenuItem implements Serializable {
     private static Logger logger = LogManager.getLogger(MenuItem.class);
@@ -35,5 +36,20 @@ public class MenuItem implements Serializable {
 
     public String getIcon() {
         return icon;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MenuItem menuItem = (MenuItem) o;
+        return Objects.equals(url, menuItem.url)
+                && Objects.equals(name, menuItem.name)
+                && Objects.equals(icon, menuItem.icon);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(url, name, icon);
     }
 }
