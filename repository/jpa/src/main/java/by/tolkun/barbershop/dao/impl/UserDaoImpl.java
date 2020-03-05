b512ef7e749bc24fb715c4b89c8d3ba6a8970a25
Kirill Tolkun <kirill@acer.localhost>
2020-03-05 23:51:27 +0300

    Add JPA support in UserDaoImpl class

--- repository/jpa/src/main/java/by/tolkun/barbershop/dao/impl/UserDaoImpl.java
+++ repository/jpa/src/main/java/by/tolkun/barbershop/dao/impl/UserDaoImpl.java
@@ -2,52 +2,113 @@ package by.tolkun.barbershop.dao.impl;
 
 import by.tolkun.barbershop.dao.UserDao;
 import by.tolkun.barbershop.entity.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.criteria.*;
 import java.util.List;
 
 @Repository
+@Transactional
 public class UserDaoImpl implements UserDao {
 
+    private final SessionFactory sessionFactory;
 
     @Autowired
-    public UserDaoImpl(final JdbcTemplate jdbcTemplate) {
-
+    public UserDaoImpl(final SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
     }
 
     @Override
     public int create(final User user) {
-        return 0;
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        int generatedId = (int) session.save(user);
+        session.getTransaction().commit();
+        return generatedId;
     }
 
     @Override
     public User read(int id) {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+        CriteriaQuery<User> criteriaQuery = criteriaBuilder.createQuery(User.class);
+        Root<User> root = criteriaQuery.from(User.class);
+        criteriaQuery.select(root).where(criteriaBuilder.equal(root.get("id"), id));
+        Query<User> query = session.createQuery(criteriaQuery);
+        return query.getSingleResult();
     }
 
     @Override
     public List<User> readAll() {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+        CriteriaQuery<User> criteriaQuery = criteriaBuilder.createQuery(User.class);
+        Root<User> root = criteriaQuery.from(User.class);
+        CriteriaQuery<User> all = criteriaQuery.select(root);
+        return session.createQuery(all).getResultList();
     }
 
     @Override
     public User read(String login) {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+        CriteriaQuery<User> criteriaQuery = criteriaBuilder.createQuery(User.class);
+        Root<User> root = criteriaQuery.from(User.class);
+        criteriaQuery.select(root).where(criteriaBuilder.equal(root.get("login"), login));
+        Query<User> query = session.createQuery(criteriaQuery);
+        return query.getSingleResult();
     }
 
     @Override
     public User read(String login, String password) {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+        CriteriaQuery<User> criteriaQuery = criteriaBuilder.createQuery(User.class);
+        Root<User> root = criteriaQuery.from(User.class);
+        criteriaQuery.select(root).where(criteriaBuilder.equal(root.get("login"), login),
+                criteriaBuilder.equal(root.get("password"), password));
+        Query<User> query = session.createQuery(criteriaQuery);
+        return query.getSingleResult();
     }
 
     @Override
     public void update(User user) {
+        Session session = sessionFactory.getCurrentSession();
+        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+        CriteriaUpdate<User> criteriaUpdate = criteriaBuilder.createCriteriaUpdate(User.class);
+        Root<User> root = criteriaUpdate.from(User.class);
+        criteriaUpdate
+                .set("login", user.getLogin())
+                .set("password", user.getPassword())
+                .set("name", user.getName())
+                .set("surname", user.getSurname())
+                .set("patronymic", user.getPatronymic())
+                .set("email", user.getEmail())
+                .set("phone", user.getPhone())
+                .set("image_path", user.getImagePath())
+                .set("role", user.getRole().getIdentity());
+        criteriaUpdate.where(criteriaBuilder.equal(root.get("id"), user.getId()));
 
+        Transaction transaction = session.beginTransaction();
+        session.createQuery(criteriaUpdate).executeUpdate();
+        transaction.commit();
     }
 
     @Override
     public void delete(int id) {
+        Session session = sessionFactory.getCurrentSession();
+        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+        CriteriaDelete<User> criteriaDelete = criteriaBuilder.createCriteriaDelete(User.class);
+        Root<User> root = criteriaDelete.from(User.class);
+        criteriaDelete.where(criteriaBuilder.equal(root.get("id"), id));
+        Transaction transaction = session.beginTransaction();
+        session.createQuery(criteriaDelete).executeUpdate();
+        transaction.commit();
     }
 }
